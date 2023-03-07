7798fd35ad85f18ee0d9e72f820a5f84ea66590b
namesty <nestor09amesty@gmail.com>
2023-03-07 03:34:31 +0100

    (feat): added functions map/table passing

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,8 @@
 use std::{collections::HashMap, ffi::CStr};
 
-extern "C" {
-  fn speak(animal: *const std::ffi::c_void, message: *const i8);
-}
-
 struct Farm {
-  pub animal_ptrs: HashMap<String, *const std::ffi::c_void>
+  pub animal_ptrs: HashMap<String, *const std::ffi::c_void>,
+  pub function_ptrs: HashMap<String, fn(&[*const std::ffi::c_void])>
 }
 
 impl Farm {
@@ -30,9 +27,31 @@ pub extern "C" fn add_animal(farm_ptr: *const std::ffi::c_void, animal_name: *co
 }
 
 #[no_mangle]
-pub extern "C" fn create_farm() -> *const std::ffi::c_void {
+pub extern "C" fn get_animal(farm_ptr: *const std::ffi::c_void, animal_name: *const i8) -> *const std::ffi::c_void {
+  let farm = unsafe {
+    Box::from_raw(farm_ptr as *mut Farm)
+  };
+
+  let animal_name_cstr = unsafe { CStr::from_ptr(animal_name) };
+  let animal_name = match animal_name_cstr.to_str() {
+      Ok(u) => u.to_string(),
+      Err(_) => panic!("Couldn't get CStr for URI")
+  };
+
+  if let Some(animal_ptr) = farm.animal_ptrs.get(&animal_name) {
+    *animal_ptr
+  } else {
+    panic!("Animal with name {} could not be found in farm", animal_name)
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn create_farm(func_ptrs: *mut HashMap<String, fn(&[*const std::ffi::c_void])>) -> *const std::ffi::c_void {
+  let functions_map = unsafe { &mut *func_ptrs };
+
   let farm = Farm {
-    animal_ptrs: HashMap::new()
+    animal_ptrs: HashMap::new(),
+    function_ptrs: *functions_map,
   };
 
   Box::into_raw(Box::new(farm)) as *const std::ffi::c_void
@@ -51,13 +70,12 @@ pub extern "C" fn native_speak(farm_ptr: *const std::ffi::c_void, animal_name: *
   };
 
   if let Some(animal_ptr) = farm.animal_ptrs.get(&animal_name) {
-    unsafe {
-      println!("Starting to speak from rust...");
-      speak(*animal_ptr, message);
-      println!("Finished speaking from rust")
-    }
+    println!("Starting to speak from rust...");
+    // TODO: handle not found
+    let speak_fn = farm.function_ptrs.get("speak").unwrap();
+    speak_fn(&[*animal_ptr, message as usize as *const std::ffi::c_void]);
+    println!("Finished speaking from rust")
   } else {
     panic!("Animal with name {} could not be found in farm", animal_name)
   }
-
 }
